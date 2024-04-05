be6c3c590fcc5fe9935dd9867bb2b73c3d4104fd
Rishik Yalamanchili <73787402+Rishik-Y@users.noreply.github.com>
2024-04-05 13:14:13 +0530

    Added Dorm

--- Sleep_Inducer.cpp
+++ Sleep_Inducer.cpp
@@ -5,6 +5,9 @@ int main()
 {
     int N; // Here N are the Number of Inmates
     cin >> N;
+    
+    int M; // Here M are the Number of sleeping dorms
+    cin >> M;
     // Your code continues here
     
     return 0;
