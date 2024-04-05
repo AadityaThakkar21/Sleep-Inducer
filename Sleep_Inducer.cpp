0b4170c308309a47e60b106277de579a1459831a
Rishik Yalamanchili <73787402+Rishik-Y@users.noreply.github.com>
2024-04-05 13:15:46 +0530

    Added Question to ask for random generation

--- Sleep_Inducer.cpp
+++ Sleep_Inducer.cpp
@@ -3,14 +3,27 @@ using namespace std;
 
 int main()
 {
-    int N; // Here N are the Number of Inmates
+    int N; // Here N is the Number of Inmates
     cin >> N;
-    
-    int M; // Here M are the Number of sleeping dorms
+
+    int M; // Here M is the Number of sleeping dorms
     cin >> M;
-    
+
     int Noofpeopleperdorm = N / M; // Here Noofpeopleperdorm is the Number of people living per dorm
-    // Your code continues here
-    
+
+    char UserRandomtaken;
+    cout << "Do you want to randomize the sleep time of inmates? Enter Y/N: ";
+    cin >> UserRandomtaken;
+
+    if (UserRandomtaken == 'Y' || UserRandomtaken == 'y') {
+        cout << "Sleep time will be randomized";
+    }
+    else if (UserRandomtaken == 'N' || UserRandomtaken == 'n') {
+        cout << "Sleep time will not be randomized";
+    }
+    else {
+        cout << "Irrelevant char entered by user";
+    }
+
     return 0;
 }
