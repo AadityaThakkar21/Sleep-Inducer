c50ef788f60f510db52fa65b89397b47a6211657
Rishik Yalamanchili <73787402+Rishik-Y@users.noreply.github.com>
2024-04-05 13:40:07 +0530

    Succesfully combined both codes

--- Sleep_Inducer.cpp
+++ Sleep_Inducer.cpp
@@ -1,4 +1,8 @@
 #include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <algorithm>
 #include <cstdlib>
 #include <ctime>
 
@@ -9,12 +13,62 @@ int randomInt(int min, int max) {
     return min + rand() % (max - min + 1);
 }
 
+// Function to generate a random array of integers
 void generateRandomArray(int Finalarr[], int N) {
     srand(static_cast<unsigned int>(time(0)));
 
     for (int i = 0; i < N; ++i) {
         Finalarr[i] = randomInt(1, 100); // Change 100 to the desired maximum value for the array elements
     }
+}
+
+// Function to update the Inmate_records.txt file
+void updateInmateRecords(int Finalarr[], int N) {
+    ifstream inFile("Inmate_records.txt");
+    if (!inFile) {
+        cerr << "Error opening input file." << endl;
+        return;
+    }
+
+    ofstream outFile("Inmate_records_updated.txt");
+    if (!outFile) {
+        cerr << "Error creating output file." << endl;
+        inFile.close();
+        return;
+    }
+
+    string line;
+    while (getline(inFile, line)) {
+        stringstream ss(line);
+        string name;
+        int earpodID;
+        vector<float> sleepTimes;
+        int p;
+        int musicID;
+
+        ss >> name >> earpodID;
+        float sleepTime;
+        for (int i = 0; i < 7; ++i) {
+            ss >> sleepTime;
+            sleepTimes.push_back(sleepTime);
+        }
+        ss >> p >> musicID;
+
+        // Rearrange sleep times according to Finalarr
+        for (int i = 0; i < 7; ++i) {
+            sleepTimes[i] = (i < 6) ? sleepTimes[Finalarr[i]] : Finalarr[6]; // Update 7th sleep time according to Finalarr[N]
+        }
+
+        // Write updated record to output file
+        outFile << name << " " << earpodID;
+        for (int i = 0; i < 7; ++i) {
+            outFile << " " << sleepTimes[i];
+        }
+        outFile << " " << p << " " << musicID << endl;
+    }
+
+    inFile.close();
+    outFile.close();
 }
 
 int main() {
@@ -30,6 +84,10 @@ int main() {
         cout << Finalarr[i] << " ";
     }
     cout << endl;
+
+    updateInmateRecords(Finalarr, N);
+
+    cout << "Inmate records updated and saved to 'Inmate_records_updated.txt'." << endl;
 
     return 0;
 }
