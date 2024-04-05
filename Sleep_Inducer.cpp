4fb98f941e7b16c515e3e70dbd5dd9b3c1fbdcd6
Physic69 <vencutsub11111111111@gmail.com>
2024-04-05 13:20:20 +0530

    Added GenerateInmateRecords function

--- Sleep_Inducer.cpp
+++ Sleep_Inducer.cpp
@@ -14,6 +14,37 @@ int randomInt(int min, int max) {
     return min + rand() % (max - min + 1);
 }
 
+void generateInmateRecords(int N) {
+    ofstream outFile("Inmate_records.txt");
+    if (!outFile) {
+        cerr << "Error creating output file." << endl;
+        return;
+    }
+
+    string names[] = {"Rishik", "Venkat", "Sara", "John", "Emma", "Alex", "Sophia", "Daniel", "Emily", "David"};
+    int numNames = sizeof(names) / sizeof(names[0]);
+
+    srand(static_cast<unsigned int>(time(0)));
+
+    for (int i = 0; i < N; ++i) {
+        string name = names[rand() % numNames];
+        int earpodID = randomInt(1000, 9999);
+        float sleepTimes[7];
+        for (int j = 0; j < 7; ++j) {
+            sleepTimes[j] = randomFloat(0.0, 24.0);
+        }
+        int p = randomInt(0, 60);
+
+        outFile << name << " " << earpodID;
+        for (int j = 0; j < 7; ++j) {
+            outFile << " " << sleepTimes[j];
+        }
+        outFile << " " << p << endl;
+    }
+
+    outFile.close();
+}
+
 int main()
 {
     int N; // Here N is the Number of Inmates
