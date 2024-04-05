ea97d4c31caa71a523052828d20c764b563b7b96
Rishik Yalamanchili <73787402+Rishik-Y@users.noreply.github.com>
2024-04-05 13:06:05 +0530

    Update Sleep_Inducer.cpp

--- Sleep_Inducer.cpp
+++ Sleep_Inducer.cpp
@@ -25,3 +25,53 @@ struct Dorm {
     vector<vector<int>> musicPlaylist;  // Music playlist for each channel
     vector<Inmate*> assignedInmates;  // Inmates assigned to the dorm
 };
+
+// Function to read input data from a file
+void readInputData(const string& filename, vector<Inmate>& inmates, vector<Dorm>& dorms) {
+    ifstream file(filename);
+    if (!file.is_open()) {
+        cerr << "Error opening file." << endl;
+        exit(1);
+    }
+
+    string line;
+    while (getline(file, line)) {
+        stringstream ss(line);
+        string type;
+        ss >> type;
+        if (type == "Inmate") {
+            Inmate inmate;
+            ss >> inmate.name >> inmate.earpodID >> inmate.fallAsleepTime;
+            for (int i = 0; i < 7; ++i) {
+                int sleepTime;
+                ss >> sleepTime;
+                inmate.sleepTime.push_back(sleepTime);
+            }
+            inmates.push_back(inmate);
+        } else if (type == "Dorm") {
+            Dorm dorm;
+            ss >> dorm.name;
+            int numChannels;
+            ss >> numChannels;
+            for (int i = 0; i < numChannels; ++i) {
+                int channel;
+                ss >> channel;
+                dorm.musicChannels.push_back(channel);
+            }
+            for (int i = 0; i < numChannels; ++i) {
+                vector<int> playlist;
+                int numSongs;
+                ss >> numSongs;
+                for (int j = 0; j < numSongs; ++j) {
+                    int songID;
+                    ss >> songID;
+                    playlist.push_back(songID);
+                }
+                dorm.musicPlaylist.push_back(playlist);
+            }
+            dorms.push_back(dorm);
+        }
+    }
+
+    file.close();
+}
