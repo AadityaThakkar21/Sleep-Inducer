2fb317bfc10f82051c87cd66cf7b973d4116ffb5
Physic69 <vencutsub11111111111@gmail.com>
2024-04-05 13:04:51 +0530

    List of #iniclude

--- Sleep_Inducer.cpp
+++ Sleep_Inducer.cpp
@@ -1,18 +1,8 @@
 #include <iostream>
-using namespace std;
-
-int addTwoNumber(int A, int B) 
-{ 
-    return A + B; 
-} 
-
-int main()
-{
-    cout << "Hello World";
-    int A;
-    int B;
-    cin >> A;
-    cin >> B;
-    cout << "sum = " << addTwoNumber(A, B); 
-    return 0;
-}
+#include <fstream>
+#include <vector>
+#include <string>
+#include <sstream>
+#include <cstdlib>
+#include <ctime>
+#include <unistd.h>
