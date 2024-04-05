00aa82e25ca76d7c128ec6fa55e88fe4bfcf4a2b
Rishik Yalamanchili <73787402+Rishik-Y@users.noreply.github.com>
2024-04-05 13:05:19 +0530

    Update Sleep_Inducer.cpp

--- Sleep_Inducer.cpp
+++ Sleep_Inducer.cpp
@@ -6,3 +6,14 @@
 #include <cstdlib>
 #include <ctime>
 #include <unistd.h>
+
+using namespace std;
+
+// Structure to store information about an inmate
+struct Inmate {
+    string name;
+    string earpodID;
+    vector<int> sleepTime;
+    int fallAsleepTime;  // Time taken by the inmate to fall asleep in minutes
+    bool asleep;  // Flag to indicate if the inmate is asleep
+};
