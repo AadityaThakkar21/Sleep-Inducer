a2ae1a30ceff3222046ae1a78ace7706145cc42b
Physic69 <vencutsub11111111111@gmail.com>
2024-04-05 13:05:39 +0530

    Update Sleep_Inducer.cpp

--- Sleep_Inducer.cpp
+++ Sleep_Inducer.cpp
@@ -17,3 +17,11 @@ struct Inmate {
     int fallAsleepTime;  // Time taken by the inmate to fall asleep in minutes
     bool asleep;  // Flag to indicate if the inmate is asleep
 };
+
+// Structure to store information about a dorm
+struct Dorm {
+    string name;
+    vector<int> musicChannels;
+    vector<vector<int>> musicPlaylist;  // Music playlist for each channel
+    vector<Inmate*> assignedInmates;  // Inmates assigned to the dorm
+};
